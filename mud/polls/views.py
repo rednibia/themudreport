64f14e7083c7c8d8370d2f4f6f8c48bf06d627a1
rednibia <andrewaibinder@gmail.com>
2016-03-21 11:21:19 -0400

    changed view

--- mud/polls/views.py
+++ mud/polls/views.py
@@ -22,20 +22,21 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
