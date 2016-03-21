f980c06de122bcccef8740b701be57382aa74b33
rednibia <andrewaibinder@gmail.com>
2016-03-21 10:53:57 -0400

    changed view

--- mud/polls/views.py
+++ mud/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from django.db.models import F
 
@@ -26,6 +27,15 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
