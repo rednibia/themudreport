40dc3d5729340721d9c1c4dc371931d28b073ede
rednibia <andrewaibinder@gmail.com>
2016-03-22 10:31:43 -0400

    trying to add base page

--- mud/mud/urls.py
+++ mud/mud/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'', include('base.urls')),
+    url(r'^$', views.index, name='index'),
 
 ]
