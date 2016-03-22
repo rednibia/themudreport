844863ca666ea0ab1becf87ca40e5da4a07b6c13
rednibia <andrewaibinder@gmail.com>
2016-03-22 10:39:18 -0400

    trying to add base page

--- mud/mud/views.py
+++ mud/mud/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main index.")
+
+def about(request):
+    return HttpResponse("This was created using Django.")
