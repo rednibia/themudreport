4b05b804de46954f591290617d861c5464216784
rednibia <andrewaibinder@gmail.com>
2016-04-13 13:20:00 -0400

    trying to add base page

--- mud/mud/views.py
+++ mud/mud/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.views import generic
+
 
 
 def index(request):
+    template_name = 'polls/index.html'
     return HttpResponse("Hello, world. You're at the main index.")
 
 def about(request):
@@ -13,3 +16,4 @@ def math(request):
         sum+=i
     answer = sum
     return HttpResponse(str(answer))
+
