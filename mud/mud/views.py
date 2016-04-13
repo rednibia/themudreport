866e67a8ee3a6cb75b5391908a4f8f68ce74a691
rednibia <andrewaibinder@gmail.com>
2016-04-13 14:24:12 -0400

    trying to use template as base page

--- mud/mud/views.py
+++ mud/mud/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
-from django.views import generic
-
+from django.shortcuts import render
 
 
 def index(request):
-    template_name = 'polls/index.html'
-    return HttpResponse("Hello, world. You're at the main index.")
+    return render('mud/index.html')
 
 def about(request):
     return HttpResponse("This was created using Django.")
