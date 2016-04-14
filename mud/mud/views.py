324f0345742ff2e380628c820c2cefeda2461ba9
rednibia <andrewaibinder@gmail.com>
2016-04-14 10:09:19 -0400

    trying to use template as base page

--- mud/mud/views.py
+++ mud/mud/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.template.loader import get_template
+from django.template import Context
 
 def index(request):
-    return render(request, 'mud/index.html', {})
-
+    t = get_template('index.html')
+    html = t.render(Context({}))
+    return HttpResponse(html)
+I
 def about(request):
     return HttpResponse("This was created using Django.")
 
