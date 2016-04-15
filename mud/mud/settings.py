5ef2f4e1d0c68c8fa3d7b08b7c9d7f9f6741100a
rednibia <andrewaibinder@gmail.com>
2016-04-15 09:24:43 -0400

    Update settings.py

--- mud/mud/settings.py
+++ mud/mud/settings.py
@@ -56,20 +56,18 @@ ROOT_URLCONF = 'mud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'mud', 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
-            'OPTIONS': {
-            'debug': DEBUG,
+        'OPTIONS': {
             'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
-    }
-    ]
+        },
+    },
+]
 
 WSGI_APPLICATION = 'mud.wsgi.application'
 
