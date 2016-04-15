75e5e070384bd23f781c0baae94ec9678780ebf8
rednibia <andrewaibinder@gmail.com>
2016-04-15 09:25:52 -0400

    Update settings.py

--- mud/mud/settings.py
+++ mud/mud/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mud', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
